f3fca769b8c716d8688a7fb8ffa1d3b0cce3aef2
charles <charles@ccxtechnologies.com>
2022-03-15 09:47:16 -0400

    add ls1012accx platform

---
+++ board/freescale/ls1012accx/ls1012accx.c
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2016 Freescale Semiconductor, Inc., 2022 CCX Technologies
+ */
+
+#include <common.h>
+#include <command.h>
+#include <fdt_support.h>
+#include <hang.h>
+#include <i2c.h>
+#include <asm/cache.h>
+#include <init.h>
+#include <asm/global_data.h>
+#include <asm/io.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/fsl_serdes.h>
+#ifdef CONFIG_FSL_LS_PPA
+#include <asm/arch/ppa.h>
+#endif
+#include <asm/arch/mmu.h>
+#include <asm/arch/soc.h>
+#include <hwconfig.h>
+#include <ahci.h>
+#include <mmc.h>
+#include <scsi.h>
+#include <fsl_esdhc.h>
+#include <env_internal.h>
+#include <fsl_mmdc.h>
+#include <netdev.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int checkboard(void)
+{
+	puts("Board: CCX Technologies DataPHY-NG ");
+	return 0;
+}
+
+int dram_init(void)
+{
+	gd->ram_size = tfa_get_dram_size();
+	if (!gd->ram_size)
+		gd->ram_size = CONFIG_SYS_SDRAM_SIZE;
+
+	return 0;
+}
+
+int board_early_init_f(void)
+{
+	fsl_lsch2_early_init_f();
+
+	return 0;
+}
+
+int board_init(void)
+{
+	struct ccsr_cci400 *cci = (struct ccsr_cci400 *)(CONFIG_SYS_IMMR +
+					CONFIG_SYS_CCI400_OFFSET);
+	/*
+	 * Set CCI-400 control override register to enable barrier
+	 * transaction
+	 */
+	if (current_el() == 3)
+		out_le32(&cci->ctrl_ord, CCI400_CTRLORD_EN_BARRIER);
+
+#ifdef CONFIG_SYS_FSL_ERRATUM_A010315
+	erratum_a010315();
+#endif
+
+#ifdef CONFIG_ENV_IS_NOWHERE
+	gd->env_addr = (ulong)&default_environment[0];
+#endif
+
+#ifdef CONFIG_FSL_LS_PPA
+	ppa_init();
+#endif
+	return 0;
+}
+
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	arch_fixup_fdt(blob);
+
+	ft_cpu_setup(blob, bd);
+
+	return 0;
+}
--- include/configs/ls1012accx.h
+++ include/configs/ls1012accx.h
@@ -1,69 +1,34 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright 2016 Freescale Semiconductor
- * Copyright 2019 NXP
+ * Copyright 2020 NXP
+ * Copyright 2016 Freescale Semiconductor, Inc.
  * Copyright 2022 CCX Technologies
  *
  */
 
-#ifndef __LS1046ACCX_H__
-#define __LS1046ACCX_H__
+#ifndef __LS1012ACCX_H__
+#define __LS1012ACCX_H__
 
-#define SPL_NO_MISC
-#include "ls1046a_common.h"
+#include "ls1012a_common.h"
 
 /* This should be incremented to force a firmware update, it isn't a
  * version inidicator but a compatibility indicator, this shouldn't
  * be updated on changes that don't break backwards compatibility */
 #define CCX_FIRMWARE_API_VERSION	"2"
 
-#define CONFIG_SYS_CLK_FREQ		100000000
-#define CONFIG_DDR_CLK_FREQ		100000000
-
-#define CONFIG_SYS_I2C_SPEED           100000
-
-#define CONFIG_LAYERSCAPE_NS_ACCESS
-
+/* DDR */
 #define CONFIG_DIMM_SLOTS_PER_CTLR	1
-/* Physical Memory Map */
-#define CONFIG_CHIP_SELECTS_PER_CTRL	4
+#define CONFIG_CHIP_SELECTS_PER_CTRL	1
+#define CONFIG_SYS_SDRAM_SIZE		0x40000000
 
-#define CONFIG_DDR_SPD
-#define SPD_EEPROM_ADDRESS		0x51
-#define CONFIG_SYS_SPD_BUS_NUM		0
-
-#define CONFIG_ENV_SIZE			0x2000
-#define CONFIG_SYS_MMC_ENV_DEV		0
-
-#ifndef SPL_NO_FMAN
-#ifdef CONFIG_SYS_DPAA_FMAN
-
-#define CONFIG_FMAN_ENET
-#define RGMII_PHY1_ADDR			0x0
-#define RGMII_PHY2_ADDR			0x3
-
-#define SGMII_PHY1_ADDR			0xc
-#define SGMII_PHY2_ADDR			0xf
-
-#define SGMII_PHY3_ADDR			0x0
-#define SGMII_PHY4_ADDR			0x3
-
-#define FDT_SEQ_MACADDR_FROM_ENV
-
-#define CONFIG_ETHPRIME			"FM1@DTSEC3"
+/*  MMC  */
+#ifdef CONFIG_MMC
+#define CONFIG_SYS_FSL_MMC_HAS_CAPBLT_VS33
 #endif
 
-#endif
+#define CONFIG_PCIE1		/* PCIE controller 1 */
 
-
-/* QSPI device */
-#ifndef SPL_NO_QSPI
-#ifdef CONFIG_FSL_QSPI
-#define CONFIG_SPI_FLASH_ISSI
-#define FSL_QSPI_FLASH_SIZE		SZ_16M
-#define FSL_QSPI_FLASH_NUM		1
-#endif
-#endif
+#define CONFIG_PCI_SCAN_SHOW
 
 #if defined(CONFIG_FUSE_MESSAGE)
 #define SD_BOOTCOMMAND "run system_fuse"
@@ -269,4 +234,4 @@
 
 #include <asm/fsl_secure_boot.h>
 
-#endif /* __LS1046ACCX_H__ */
+#endif /* __LS1012ACCX_H__ */
