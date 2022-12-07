12f98fd0c285b14599e3f1a22eec3d08208f6f73
charles <charles@ccxtechnologies.com>
2022-12-07 12:59:19 -0500

    toggle dataphy resets at power-up

--- include/configs/ls1012accx.h
+++ include/configs/ls1012accx.h
@@ -14,7 +14,7 @@
 /* This should be incremented to force a firmware update, it isn't a
  * version inidicator but a compatibility indicator, this shouldn't
  * be updated on changes that don't break backwards compatibility */
-#define CCX_FIRMWARE_API_VERSION	"5"
+#define CCX_FIRMWARE_API_VERSION	"6"
 
 /* DDR */
 #define CONFIG_DIMM_SLOTS_PER_CTLR		1
